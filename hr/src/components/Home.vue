71ffa85cb48c8a6ec2e854a6bf3f0e51f0e7686d
mr.somebody <mr.somebody@mrsomebodydeMacBook-Air.local>
2021-06-26 10:56:09 +0800

    readme

---
+++ hr/src/components/Home.vue
@@ -0,0 +1,212 @@
+<template>
+
+    <el-container class="home-container">
+        <!-- 头部区域 -->
+        <el-header>
+            <div>
+                <span style="text-align:center;line-height:18px;" @click="backHome">人事管理系统</span>
+            </div>
+          <div>
+          <el-button :type="buttonType(index)" ref="sign" @click="sign" :disabled="isDisabled">{{signParam}}</el-button>
+          <el-button type="danger" @click="logout">退出</el-button>
+          </div>
+
+        </el-header>
+        <el-container>
+            <!-- 侧边栏 -->
+            <el-aside :width="isCollapse? '5%':'15%'">
+                <div class="toggle-button" @click="toggleCollapse">|||</div>
+                <el-menu
+                    :unique-opened="true" :router="true" :collapse="isCollapse" :collapse-transition="false"
+                    class="el-menu-vertical-demo" :default-active="activePath"
+                    background-color="#272C37"
+                    text-color="#fff"
+                    active-text-color="#409eff">
+                    <!-- 一级菜单 -->
+                    <el-submenu v-for="item in menuList" :key="item.id" :index="item.id" >
+                        <template slot="title">
+                        <i :class="item.icon"></i>
+                        <span>{{item.name}}</span>
+                        </template>
+                        <el-menu-item v-for="subitem in item.children" :key="subitem.id" :index="subitem.path"
+                        @click="saveNavState(subitem.path)">
+                            <template slot="title">
+                            <i :class="subitem.icon"></i>
+                            <span>{{subitem.name}}</span>
+                            </template>
+                        </el-menu-item>
+                    </el-submenu>
+                </el-menu>
+            </el-aside>
+            <!-- 主体区域 -->
+            <el-main>
+                <router-view>
+                  <div class="im">
+
+                  </div>
+                </router-view>
+            </el-main>
+        </el-container>
+    </el-container>
+</template>
+<script>
+export default {
+  inject: ['reload'],
+  data () {
+    return {
+      menuList: [],
+      isCollapse: false,
+      activePath: '',
+      signParam: '',
+      isDisabled: false,
+      signFlag: '',
+      index: ''
+    }
+  },
+  created () {
+    this.getMenu()
+    this.activePath = window.sessionStorage.getItem('activePath')
+  },
+  methods: {
+    buttonType (index) {
+      console.log(this.index)
+      // eslint-disable-next-line eqeqeq
+      if (index == 1) {
+        return 'success'
+        // eslint-disable-next-line eqeqeq
+      } else if (index == 2) {
+        return 'warning'
+        // eslint-disable-next-line eqeqeq
+      } else if (index == 3) {
+        return 'info'
+      }
+    },
+    getCurrentTime () {
+      var yy = new Date().getFullYear()
+      var mm = new Date().getMonth() + 1
+      var dd = new Date().getDate()
+      var hh = new Date().getHours()
+      var mf = new Date().getMinutes() < 10 ? '0' + new Date().getMinutes() : new Date().getMinutes()
+      var ss = new Date().getSeconds() < 10 ? '0' + new Date().getSeconds() : new Date().getSeconds()
+      this.gettime = yy + '-' + mm + '-' + dd + ' ' + hh + ':' + mf + ':' + ss
+      console.log(this.gettime)
+    },
+    sign () {
+      if (this.signFlag === 202) {
+        this.getCurrentTime()
+        this.$http.post('/user/signIn')
+          .then(res => {
+            this.$message.success(' 签到成功 ' + this.gettime)
+            console.log(' 签到成功 ' + this.gettime)
+            this.signParam = '签退'
+            this.index = 2
+            this.signFlag = 200
+            this.reload()
+            console.log(res)
+          })
+          .catch(error => {
+            this.$message.error('签到出错啦')
+            console.log(error)
+          })
+      }
+      if (this.signFlag === 200) {
+        this.getCurrentTime()
+        this.$http.post('/user/signUp')
+          .then(res => {
+            this.$message.success(' 签退成功 ' + this.gettime)
+            console.log(' 签退成功 ' + this.gettime)
+            this.signParam = '已签退'
+            this.signFlag = 201
+            this.index = 3
+            this.isDisabled = true
+            this.reload()
+            console.log(res)
+          })
+          .catch(error => {
+            this.$message.error('签退出错啦')
+            console.log(error)
+          })
+      }
+    },
+    // 获取菜单
+    getMenu () {
+      this.$http.get('/getmenu')
+        .then(res => {
+          const data = res.data.data
+          this.menuList = data
+          this.signFlag = res.data.sign
+          this.signParam = res.data.signNotes
+          if (res.data.sign === 202) {
+            this.index = 1
+          } else if (res.data.sign === 201) {
+            this.index = 3
+            this.isDisabled = true
+          } else if (res.data.sign === 200) {
+            this.index = 2
+          }
+          // this.reload()
+        })
+        // this.reload()
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    // 退出
+    logout () {
+      sessionStorage.clear()
+      this.$router.push('/login')
+    },
+    // 点击按钮切换菜单的折叠与展开
+    toggleCollapse () {
+      this.isCollapse = !this.isCollapse
+    },
+    // 返回主页而
+    backHome () {
+      this.$router.push('/home')
+    },
+    // 保存链接的激活状态
+    saveNavState (activePath) {
+      window.sessionStorage.setItem('activePath', activePath)
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.home-container{
+    height: 100%;
+}
+.el-header{
+    background-color: #272C37;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 0;
+    align-items: center;
+    color:#fff;
+    font-size: 25px;
+    >div{
+        display: flex;
+        align-items: center;
+        span{
+            margin-left: 10px;
+        }
+    }
+}
+.el-aside{
+    background-color: #272C37;
+    .el-menu{
+        border-right: none;
+    }
+}
+.el-main{
+    background-color:#fff;
+}
+.toggle-button{
+    background-color: #272C37;
+    color: #fff;
+    font-size: 10px;
+    line-height: 24px;
+    text-align: center;
+    letter-spacing: 0.2em;
+    cursor: pointer;
+}
+</style>
---
+++ hr/src/components/Login.vue
@@ -0,0 +1,175 @@
+<template>
+    <div class="login_container">
+      <vue-particles
+        class="login-bg"
+        color="#dedede"
+        :particleOpacity="0.7"
+        :particlesNumber="60"
+        shapeType="circle"
+        :particleSize="4"
+        linesColor="#dedede"
+        :linesWidth="1"
+        :lineLinked="true"
+        :lineOpacity="0.5"
+        :linesDistance="150"
+        :moveSpeed="3"
+        :hoverEffect="true"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="push"
+      >
+      </vue-particles>
+      <div class="login_container1">
+            <span class="title_span">人事管理系统</span>
+                <el-form ref="loginFormRef" :model="loginForm"  label-width="" class="login_form" :rules="loginFormRules">
+                    <el-form-item label="" prop="username">
+                        <div class="search1">
+                        <el-input v-model="loginForm.username" prefix-icon="el-icon-user" placeholder="请输入用户名"></el-input>
+                        </div>
+                    </el-form-item>
+                    <el-form-item label="" prop="password">
+                      <div class="search2">
+                        <el-input v-model="loginForm.password" prefix-icon="el-icon-lock" type="password" placeholder="请输入密码"></el-input>
+                      </div>
+                    </el-form-item>
+                    <el-form-item class="btns">
+                      <div class="button">
+                        <router-link to="/register">
+                          <el-button round >注册</el-button>
+                        </router-link>
+                        <el-button round @click="login">登录</el-button>
+                        <el-button type="info" round @click="reset">重置</el-button>
+                      </div>
+                    </el-form-item>
+                </el-form>
+      </div>
+    </div>
+</template>
+
+<script>
+import Qs from 'qs'
+import Vue from 'vue'
+import VueParticles from 'vue-particles'
+Vue.use(VueParticles)
+
+export default {
+  data () {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: '用户名不能为空', trigger: 'blur' },
+          { min: 3, max: 20, message: '用户名必须在3-20个字符之间', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '密码不能为空', trigger: 'blur' },
+          { min: 3, max: 20, message: '密码必须在3-20个字符之间', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+    // 先进行表单预验证
+      this.$refs.loginFormRef.validate(async valid => {
+        if (valid === true) {
+        // 表单预验证通过，向后台发送请求
+          this.$http.post('login', Qs.stringify(this.loginForm))
+            .then(res => {
+              console.log(res.data)
+              const token = res.data.token
+              this.$message.success('登录成功')
+              // 保存token
+              sessionStorage.setItem('token', token)
+              this.$router.push('/home')
+              // this.reload()
+            })
+            .catch(error => {
+              const error2 = error + ''
+              if (error2.indexOf('401') !== -1) { this.$message.error('用户名或密码错误') } else this.$message.error('服务器出错了！')
+            })
+        }
+      })
+    },
+    reset () {
+      this.$refs.loginFormRef.resetFields()
+      this.$message.success('已重置登录框')
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.login_container{
+    display:flex;
+    flex-direction: column;
+    justify-content:center;
+    align-items:center;
+    background:url(../assets/demo-1-bg.jpg)no-repeat fixed top;
+    background-color: #555;
+    height: 100%;
+    background-size: 100% 100%;
+
+}
+#particles-js{
+  width: 100%;
+  height: calc(100%);
+  position: absolute;
+}
+.login_container1{
+  display:flex;
+  flex-direction: column;
+  justify-content:center;
+  align-items:center;
+  background-color: rgba(255,255,255,0.1);
+  height: 500px;
+  width: 350px;
+  background-size: 100% 100%;
+}
+.title_span{
+    position:relative;
+    display:flex;
+    flex-direction: column;
+    color: floralwhite;
+    font-size: 35px;
+    font-family: "楷体",sans-serif;
+    top: -10%;
+}
+.search1{
+     display: flex;
+     flex-direction: column;
+     justify-content: center;
+     align-items: center;
+    .el-input {
+      width: 100%;
+    }
+
+}
+.search2{
+     display: flex;
+     flex-direction: column;
+     justify-content: center;
+     align-items: center;
+    .el-input {
+      width: 100%;
+    }
+}
+.button {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.btns{
+    display:flex;
+    flex-direction: column;
+}
+.login_form{
+    width: 100%;
+    padding: 10px 20px;
+    box-sizing: border-box;
+
+}
+</style>
